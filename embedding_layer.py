6154d6ed402edcc62dc950ecd0de8c6518e01b1f
Gukhwan Hyun <gusrnrghks@gmail.com>
2024-03-27 22:09:06 +0900

    add embedding process

--- embedding_layer.py
+++ embedding_layer.py
@@ -1,16 +1,58 @@
-import torch.nn as nn
 import math
+import torch.nn as nn
+import torch
+from utils import tokenize, build_vocab
 
-class InputEmbedding(nn.Module):
 
-    def __init__(self, d_model: int, vocab_size: int) -> None:
+class Embeddings(nn.Module):
+    def __init__(self, vocab_size: int, d_model: int):
         super().__init__()
+        self.look_up_table = nn.Embedding(vocab_size, d_model)
         self.d_model = d_model
-        self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size, d_model)
 
     def forward(self, x):
         # Paper mentioned to multiply by sqrt(d_model) to scale the embeddings
-        return self.embedding(x) * math.sqrt(self.d_model)
+        return self.look_up_table(x) * math.sqrt(self.d_model)
+
+
+if __name__ == '__main__':
+
+    # list of sequences (3,)
+    sequences = ["I wonder what will come next!",
+                 "This is a basic example paragraph.",
+                 "Hello, what is a basic split?"]
+
+    # tokenize the sequences -> (batch_size, seq_length) -> (3,6)
+    tokenized_sequences = [tokenize(seq) for seq in sequences]
+    print(tokenized_sequences)
+
+    # concatenate the sequences
+    sequence_cluster = " ".join(sequences)
+    print(sequence_cluster)
+
+    # build the vocabulary (corpus)
+    word_table = build_vocab(sequence_cluster)
+    print(word_table)
+
+    # Integer encoding
+    encoded_sequence = [word_table[word] for word in tokenize(sequence_cluster)]
+    print(encoded_sequence)
+
+    vocab_size = len(word_table)
+    print(vocab_size)
+
+    # set model embedding dimensions
+    d_model = 3
+
+    # create the initial embedding layer
+    # lookuptable = torch.rand(vocab_size, d_model)  # matrix of size (14, 3)
+    lookuptable = nn.Embedding(vocab_size, d_model)
+    print(lookuptable.state_dict()['weight'])
+
+    # apply embedding
+    indices = torch.Tensor(encoded_sequence).long()  # (batch_size, seq_length, d_model)
+    # embeddings = embeddings[encoded_sequence]
+    embeddings = lookuptable(indices)
+    print(embeddings)
 
 
--- utils.py
+++ utils.py
@@ -18,4 +18,27 @@ def apply_softmax_function(logtis):
     return nn.Softmax(dim=1)(logtis)
 
 
+def tokenize(sequence):
+    # remove punctuation
+    for punc in ["!", ".", "?"]:
+        sequence = sequence.replace(punc, "")
+
+    # split the sequence on spaces and lowercase each token
+    return [token.lower() for token in sequence.split(" ")]
+
+
+def build_vocab(data):
+    # tokenize the data and remove duplicates
+    vocab = list(set(tokenize(data)))
+
+    # sort the vocabulary
+    vocab.sort()
+
+    # assign an integer to each word
+    look_up_table = {word: i for i, word in enumerate(vocab)}
+
+    return look_up_table
+
+
+
 
